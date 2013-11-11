a81f88ae77098b4d52d878fac8a15834aaf6d7f7
Jeffrey Paul <sneak@datavibe.net>
2013-11-11 12:12:54 +0100

    refactoring and updating

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ sync:
 
 build:
 	./build.sh
+
+visualize:
+	$(eval TEMPD := $(shell mktemp -d -t XXXXX))
+	cd $(TEMPD) && \
+		ssh $(DOCKERHOST) "docker images -viz" | dot -Tpdf -o x.pdf && \
+		open x.pdf
--- golang/run.sh
+++
@@ -1,7 +0,0 @@
-cd /usr/lib && wget -O - http://go.googlecode.com/files/go1.2rc1.src.tar.gz | tar zxvf -
-cd /usr/lib/go/src && /bin/bash all.bash
-echo 'export PATH="$PATH:/usr/lib/go/bin"' > \
-    /etc/profile.d/go.sh
-echo 'export GOHOME="/usr/lib/go"' >> \
-    /etc/profile.d/go.sh
-chmod +x /etc/profile.d/go.sh
